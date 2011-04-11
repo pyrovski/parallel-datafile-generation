126555c9f4b19b4a68ca1298dfef73585758e286
Peter Bailey <peter.eldridge.bailey@gmail.com>
2011-04-11 15:19:58 -0700

    read file

--- makefile
+++ makefile
@@ -2,7 +2,7 @@ targets=gen read
 all: $(targets)
 .PHONY: clean
 clean:
-	rm -f *.~ $(targets)
+	rm -f *.~ $(targets) *.o
 
 read: read.cpp
 	mpic++ -o $@ $^ 
--- read.cpp
+++ read.cpp
@@ -4,16 +4,17 @@
 #include <iostream>
 #include <fstream>
 #include <mpi.h>
+#include <sys/time.h>
 
 using namespace std;
 int main(int argc, char **argv){
   int retVal;
+  MPI_Init(&argc, &argv);
   if(argc < 2){
     cout << "usage: " << argv[0] << " <input file> <rows> <colums>" << endl;
     MPI_Abort(MPI_COMM_WORLD, 1);
   }
 
-  MPI_Init(&argc, &argv);
   
   int id, numProcs;
   MPI_Comm_rank(MPI_COMM_WORLD, &id);
@@ -21,8 +22,21 @@ int main(int argc, char **argv){
   FILE *file = fopen(argv[1], "r");
   unsigned rows = atoi(argv[2]);
   unsigned cols = atoi(argv[3]);
-  long offset = id * rows * cols * sizeof(double);
+  unsigned myCols = cols / numProcs;
+  unsigned colStart = id * myCols;
+  unsigned colEnd = colStart + myCols;
+  long offset = id * colStart * rows * sizeof(double);
   int status = fseek(file, offset, SEEK_SET);
+  
+  // just read one column at a time (assume column-major)
+  struct timeval tStart, tEnd;
+  gettimeofday(&tStart, 0);
+  double *array = (double*)malloc(rows * sizeof(double));
+  for(unsigned col = colStart; col < colEnd; col++){
+    fread(array, sizeof(double), rows, file);
+  }
+  
+  gettimeofday(&tEnd, 0);
 
   MPI_Finalize();
   return 0;
